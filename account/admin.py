38339433d03122584a6d947a2ce900ca9d89c8c9
lch-dewang <690501772@qq.com>
2018-07-05 09:22:41 +0800

    add

--- account/admin.py
+++ account/admin.py
@@ -11,9 +11,9 @@ class MyUserAdmin(UserAdmin):
     add_form = MyUserCreationForm
     # The fields to be used in displaying the User model.
     fieldsets = (
-        (None, {'fields': ('uid','uname','ugroup','password')}),
+        (None, {'fields': ('uid','uname','Group','password')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups','upermission')}),
+                                       'Group','upermission')}),
 #         ('Important dates', {'fields': ('qq_name', 'type', 'level', 'profile', 'with_total','accu_income', 'date_joined', )}),
 #         ('others', {'fields': ('balance', 'margin_account','domain_name', 'cs_qq', 'invite_code', 'inviter','zhifubao')}),
     )
