ddb645011cd2b36bc25e0ef41883f9aadb9e90fd
kevinyings <kevinyings@outlook.com>
2018-07-09 15:50:07 +0800

    add Prostatis

--- prostatis/views.py
+++ prostatis/views.py
@@ -13,46 +13,46 @@ import django_filters
 import datetime
 
 
-# Create your views here.
-# class DayStatisList(generics.ListCreateAPIView):
-#     queryset = DayStatis.objects.all()
-#     serializer_class = DayStatisSerializer
-#     pagination_class = MyPageNumberPagination
-#
-# class ProjectDetailList(generics.ListCreateAPIView):
-#     queryset = DayStatis.objects.all()
-#     serializer_class = DayStatisSerializer
-#     pagination_class = MyPageNumberPagination
+#Create your views here.
+class DayStatisList(generics.ListCreateAPIView):
+    queryset = DayStatis.objects.all()
+    serializer_class = DayStatisSerializer
+    pagination_class = MyPageNumberPagination
+
+class ProjectDetailList(generics.ListCreateAPIView):
+    queryset = DayStatis.objects.all()
+    serializer_class = DayStatisSerializer
+    pagination_class = MyPageNumberPagination
 
 
-# class ProStatis(viewsets.ModelViewSet):
-#     queryset = ProjectInvestDataModel.objects.all()
-#     serializer_class = ProjectInvestDataSerializer
-#     @action(methods=['get'],detail=False)
-#     def get_date(self, request, pk=None,*args,**kwargs):
-#         '''　项目详情:到项目详情　　单个项目的每天信息　日期   当前总待收   预估利润    昨日结算金额  '''
-#         cursor = connection.cursor()
-#         cursor.execute("select  a.project_id, \
-#                                 b.consume-b.settle,\
-#                                 a.source,\
-#                                 a.audit_time,\
-# 		                        a.settle_amount,\
-# 		                        sum(a.settle_amount) as sumofsettle,\
-# 		                        sum(a.return_amount) as sumofret,\
-# 		                        b.paccountype as accounttype,\
-# 		                        sum(case b.paccountype when '0' then a.settle_amount*0.94 -a.return_amount  else  a.settle_amount -a.return_amount end )\
-#                         from project_projectinvestdatamodel as a\
-#                         left join project_project as b on b.id = a.project_id\
-#                         group by a.project_id, a.audit_time")
-#         row = cursor.fetchall()
-#         returndict={}
-#         for item in row:
-#             returndict['date']=item[2]
-#             returndict['currenttopay']=item[1]
-#             returndict['preprofit']=item[7]
-#
-#
-#         return Response(returndict)
+class ProStatis(viewsets.ModelViewSet):
+    queryset = ProjectInvestDataModel.objects.all()
+    serializer_class = ProjectInvestDataSerializer
+    @action(methods=['get'],detail=False)
+    def get_date(self, request, pk=None,*args,**kwargs):
+        '''　项目详情:到项目详情　　单个项目的每天信息　日期   当前总待收   预估利润    昨日结算金额  '''
+        cursor = connection.cursor()
+        cursor.execute("select  a.project_id, \
+                                b.consume-b.settle,\
+                                a.source,\
+                                a.audit_time,\
+		                        a.settle_amount,\
+		                        sum(a.settle_amount) as sumofsettle,\
+		                        sum(a.return_amount) as sumofret,\
+		                        b.paccountype as accounttype,\
+		                        sum(case b.paccountype when '0' then a.settle_amount*0.94 -a.return_amount  else  a.settle_amount -a.return_amount end )\
+                        from project_projectinvestdatamodel as a\
+                        left join project_project as b on b.id = a.project_id\
+                        group by a.project_id, a.audit_time")
+        row = cursor.fetchall()
+        returndict={}
+        for item in row:
+            returndict['date']=item[2]
+            returndict['currenttopay']=item[1]
+            returndict['preprofit']=item[7]
+
+
+        return Response(returndict)
 
 
 
