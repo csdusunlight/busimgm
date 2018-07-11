908f7b968b5d9c593da78b31a8d5d8a98fa40d54
lvchunhui7 <lvchunhui7@126.com>
2018-07-11 17:48:02 +0800

    statis

--- prostatis/management/commands/day_task.py
+++ prostatis/management/commands/day_task.py
@@ -2,21 +2,21 @@
 import datetime
 
 from django.core.management import BaseCommand
-from django.db.models import Sum, Count, Q
 
 from account.models import User
 from prostatis.models import ProjectDayStatis, UserDayStatis, DayStatis, UserStatis
-from project.models import ProjectInvestDataModel, Project
+from project.models import Project
 
 
-# class Command(BaseCommand):
-    # def handle(self, *args, **options):
-    #     users = User.objects.all()
-    #     today = datetime.date.today()
-    #     projects = Project.objects.all(state)
-    #     for user in users:
-    #         ProjectDayStatis.objects.create(project_id=project_id, date=date,
-    #                 defaults=item)
-    #     UserDayStatis.objects.update_or_create(user_id=user_id, date=date, defaults=item)
-    #     DayStatis.objects.update_or_create(date=date,defaults=item)
-    #     UserStatis.objects.update_or_create(user_id=user_id, defaults=item)
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        users = User.objects.all()
+        today = datetime.date.today()
+        projects = Project.objects.filter(state__in=['01','04','06'])
+        for project in projects:
+            ProjectDayStatis.objects.update_or_create(project=project, date=today)
+        for user in users:
+            UserDayStatis.objects.update_or_create(user=user, date=today)
+            UserStatis.objects.update_or_create(user=user)
+        DayStatis.objects.update_or_create(date=today)
+
