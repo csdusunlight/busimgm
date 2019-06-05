11ec91b4cc0d5d68036d4642495c73c0cdd8e32f
jiangzongyang <jiangzongyang197@163.com>
2019-06-05 10:10:50 +0800

    ie bug

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -22,7 +22,8 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    /* app: './src/main.js' */
+    app: ["babel-polyfill", "./src/main.js"]
   },
   output: {
     path: config.build.assetsRoot,
@@ -54,7 +55,8 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        /* include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')], */
+        include: [resolve('src'), resolve('test'),resolve('/node_modules/element-ui/src'),resolve('/node_modules/element-ui/packages')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
